2191bf1b4f8eea00f7e8bfee3165c406694417d5
takeyu1013 <yuto1013@gmail.com>
2023-10-09 07:35:46 +0000

    🔧

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import tsEsLintPlugin from "@typescript-eslint/eslint-plugin";
+import tsEsLintParser from "@typescript-eslint/parser";
+
+export default [
+  js.configs.recommended,
+  {
+    plugins: {
+      "@typescript-eslint": tsEsLintPlugin,
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.cts", "**/*.mts"],
+    languageOptions: {
+      parser: tsEsLintParser,
+      parserOptions: {
+        project: true,
+      },
+    },
+    rules: {
+      ...tsEsLintPlugin.configs["eslint-recommended"].rules,
+      ...tsEsLintPlugin.configs["recommended-type-checked"].rules,
+      "@typescript-eslint/no-explicit-any": "error",
+    },
+  },
+];
