263ae6ca6141f86eaa47b4a73491ea1abb1b038c
takeyu1013 <yuto1013@gmail.com>
2023-10-10 14:18:44 +0000

    👔

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
+import { PrismaClient } from "@prisma/client";
 import { Hono } from "hono";
+import { serve } from "@hono/node-server";
 
+const prisma = new PrismaClient();
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello Hono!"));
 
-export default app;
+const route = app.get("/users", async (c) => {
+  const users = await prisma.user.findMany();
+  return c.jsonT(users);
+});
+
+export type AppType = typeof route;
+
+serve(app);
