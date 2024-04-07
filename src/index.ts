015601252975dcd3c114c8550512845c7030cf88
takeyu1013 <yuto1013@gmail.com>
2024-04-07 14:12:11 +0000

    ✨

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,10 @@ import { createRoute } from "@hono/zod-openapi";
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
 import { PrismaClient } from "@prisma/client";
+import { GitHub, generateState } from "arctic";
+import { getCookie } from "hono/cookie";
 import { Lucia } from "lucia";
+import { serializeCookie } from "oslo/cookie";
 import { Argon2id } from "oslo/password";
 
 const client = new PrismaClient();
@@ -17,10 +20,11 @@ const lucia = new Lucia(adapter, {
 			secure: process.env.NODE_ENV === "production",
 		},
 	},
-	getUserAttributes: ({ email }) => {
+	getUserAttributes: ({ email, githubId }) => {
 		return {
 			// attributes has the type of DatabaseUserAttributes
 			email,
+			githubId,
 		};
 	},
 });
@@ -34,7 +38,12 @@ declare module "lucia" {
 
 interface DatabaseUserAttributes {
 	email: string;
+	githubId: number;
 }
+export const github = new GitHub(
+	process.env.GITHUB_CLIENT_ID ?? "",
+	process.env.GITHUB_CLIENT_SECRET ?? "",
+);
 
 const app = new OpenAPIHono();
 
@@ -91,6 +100,57 @@ app.openapi(
 		}
 		const { id } = await lucia.createSession(user.id, {});
 		return json({ token: id });
+	},
+);
+app.openapi(
+	createRoute({
+		method: "get",
+		path: "/login/github",
+		responses: { 301: { description: "" } },
+	}),
+	async (context) => {
+		const state = generateState();
+		const url = await github.createAuthorizationURL(state);
+		context.res.headers.append(
+			"Set-Cookie",
+			serializeCookie("github_oauth_state", state, {
+				path: "/",
+				secure: process.env.NODE_ENV === "production",
+				httpOnly: true,
+				maxAge: 60 * 10,
+				sameSite: "lax",
+			}),
+		);
+		return context.redirect(url.toString(), 301);
+	},
+);
+app.openapi(
+	createRoute({
+		method: "get",
+		path: "/login/github/callback",
+		responses: {
+			200: {
+				content: {
+					"application/json": {
+						schema: z.object({
+							token: z.string(),
+						}),
+					},
+				},
+				description: "",
+			},
+		},
+	}),
+	async (context) => {
+		const url = new URL(context.req.url);
+		const code = url.searchParams.get("code");
+		const state = url.searchParams.get("state");
+		const storedState = getCookie(context, "github_oauth_state");
+		if (!code || !state || !storedState || state !== storedState) {
+			return context.json({ token: "" });
+		}
+		const { accessToken } = await github.validateAuthorizationCode(code);
+		return context.json({ token: accessToken });
 	},
 );
 
