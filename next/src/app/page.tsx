ef15badb4f46a2df946a33dbfd667ebc9bc48002
takeyu1013 <yuto1013@gmail.com>
2024-10-20 06:22:48 +0000

    🔧

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Text } from "@mantine/core";
 import { hc } from "hono/client";
 
 import type { route } from "@/app/api/[...route]/route";
@@ -10,5 +11,9 @@ export default async function Home() {
 	} = hc<typeof route>("http://localhost:3000");
 	const { message } = await (await $get()).json();
 
-	return <main>{message}</main>;
+	return (
+		<main>
+			<Text>{message}</Text>
+		</main>
+	);
 }
